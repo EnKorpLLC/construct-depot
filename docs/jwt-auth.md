a87bac9d4dc59a2e6c4c607220685bacda97e6f1
EnKorpLLC <team@enkorpllc.com>
2025-01-29 16:03:03 -0600

    Initial Next.js setup

--- docs/jwt-auth.md
+++
@@ -1,9 +0,0 @@
-# Jwt Auth
-
-Last Updated: 2025-01-21 20:34
-
- 
-## Overview
-
-This document provides information about jwt-auth.
-
--- frontend/src/index.ts
+++
@@ -1,6 +0,0 @@
-import { startServer } from './server';
-
-startServer().catch((err) => {
-  console.error('Failed to start server:', err);
-  process.exit(1);
-}); 
